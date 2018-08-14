3f3014c9c0ace0a7bf8f6754360d7be94ca2ad52
fongvang <fvang1020@gmail.com>
2018-08-14 14:07:11 -0700

    I added mongoose schema to server.js file

--- server.js
+++ server.js
@@ -2,13 +2,31 @@ const express = require('express');
 const bp = require('body-parser');
 const path = require('path');
 var app = express();
-
-
+var mongoose = require('mongoose');
 
 app.use(express.static(path.join(__dirname+"/client/dist/client")));
 app.use(bp.json());
 app.use(bp.urlencoded({extended:true}));
 
+// -----------MONGOOSE CONNECT & SCHEMA -----------------------
+mongoose.connect('mongodb://localhost:27017/ninja_gold', { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+
+var NinjaSchema = new mongoose.Schema({
+    name: {type: String, required: true, minlength: 1},
+    gold: {type: Number},
+}, {timestamps: true})
+
+mongoose.model('Ninja', NinjaSchema);
+var Ninja = mongoose.model('Ninja');
+
+// ------------ ROUTES AND LOCATIONS BELOW -------------------
+
+
+
+
+
+//----------PORT LISTENER ------------------------------------
 app.listen(8000,function(){
     console.log("Now serving on 8000");
 })
